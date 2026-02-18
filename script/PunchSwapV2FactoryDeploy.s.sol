7e6cce74bfab46994e1511d90807d04807a404a6
Spicy Kitty <spicykitty@kittypunch.xyz>
2026-02-18 09:51:58 -0600

    feat: improve script

--- script/PunchSwapV2FactoryDeploy.s.sol
+++ script/PunchSwapV2FactoryDeploy.s.sol
@@ -18,13 +18,19 @@ contract PunchSwapV2FactoryDeployScript is Script {
     function run() public {
         uint256 deployerPrivateKey = vm.envUint(PARAM_PK_ACCOUNT);
         address _owner = vm.envAddress(PARAM_OWNER);
-        address _feeTo = _owner;
-        console.log("Owner address:             ", _owner);
+        address _initialFeeToSetter = _owner;
+        address _finalFeeToSetter = address(0x0);
+        address _feeTo = address(0x0);
+
+        console.log("Owner:                     ", _owner);
+        console.log("Fee To:                    ", _feeTo);
+        console.log("Initial Fee To Setter:     ", _initialFeeToSetter);
+        console.log("Final Fee To Setter:       ", _finalFeeToSetter);
 
         console.log("Starting script: broadcasting");
         vm.startBroadcast(deployerPrivateKey);
 
-        PunchSwapV2Factory instance = new PunchSwapV2Factory(_feeTo);
+        PunchSwapV2Factory instance = new PunchSwapV2Factory(_initialFeeToSetter);
 
         console.log("PunchSwapV2Factory:    ", address(instance));
         console.log("PunchSwapV2Factory.INIT_CODE_PAIR_HASH:  ");
@@ -41,6 +47,14 @@ contract PunchSwapV2FactoryDeployScript is Script {
             "PunchSwapV2Factory.INIT_CODE_PAIR_HASH != PunchSwapV2Pair keccak256(bytecode)"
         );
         console.log("Bytecode hashcode verified");
+
+        instance.setFeeTo(_feeTo);
+        console.log("Fee To:                    ", instance.feeTo());
+
+        if (_finalFeeToSetter != _initialFeeToSetter) {
+            instance.setFeeToSetter(_finalFeeToSetter);
+            console.log("Fee To Setter:             ", instance.feeToSetter());
+        }
 
         vm.stopBroadcast();
     }
